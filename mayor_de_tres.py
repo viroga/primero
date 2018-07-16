0ea1ad3c10e0a2b19b19132265cd32145bbf5b68
viroga <victorrg10@gmail.com>
2018-07-16 20:41:29 +0200

    Test_input and print

---
+++ mayor_de_tres.py
@@ -0,0 +1,6 @@
+primer_numero = int(input("primer numero: "))
+segundo_numero = int(input("segundo numero: "))
+tercer_mumero = int (input("segundo numero: "))
+resultado = max(primer_numero, segundo_numero, tercer_mumero)
+print ("El numero mas grande es: {} ".format(resultado))
+
