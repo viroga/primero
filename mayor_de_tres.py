fd77e4726edf8f542568e194cd46c573bca4dca8
viroga <victorrg10@gmail.com>
2018-07-16 21:52:09 +0200

    Test_input and print

--- mayor_de_tres.py
+++ mayor_de_tres.py
@@ -1,6 +1,4 @@
-primer_numero = int(input("primer numero: "))
-segundo_numero = int(input("segundo numero: "))
-tercer_mumero = int (input("segundo numero: "))
-resultado = max(primer_numero, segundo_numero, tercer_mumero)
-print ("El numero mas grande es: {} ".format(resultado))
-
+nombre_de_usuario = input("Nombre: ")
+contrasenya = input("Contraseña: ")
+confirmar_contrasenya = input("Confirmar contraseña: ")
+print ("Registro completado").format (nombre_de_usuario, contrasenya, confirmar_contrasenya)
