9084951c39793fa5e9df2b5e5e2a472e6bd52977
viroga <victorrg10@gmail.com>
2018-07-19 08:47:40 +0200

    Test_input and print

--- comer_helado.py
+++ comer_helado.py
@@ -1,11 +1,11 @@
-apetece_helado = input ("¿Te apetece un helado? (Si / No) : ")
-tiene_dinero = input ("¿Puedes pagarlo?  (Si / No) : ")
-de_que_sabor = input ("¿De que sabor? (Choolate/Vainilla: ")
+apetece_helado = input ("¿Te apetece un helado? (Si / No): ")
+tiene_dinero = input ("¿Puedes pagarlo?  (Si / No): ")
+de_que_sabor = input ("¿De que sabor? (Chocolate / Vainilla: ")
+tu_madre_te_deja = input("¿Tu madre te deja? (Si / No: ")
+if apetece_helado == "Si" and tiene_dinero == "Si" and  de_que_sabor == "Chocolate / Vainilla":
 
-if apetece_helado == "Si" and tiene_dinero == "Si":
-    print ("¿De que sabor?")
-if de_que_sabor == "Chocolate , Vainilla":
-    (print) ("aqui tienes")
+        (print)("aqui tienes")
 
-else :
-    print ("Que pases un buen dia")
+else:
+     print ("Largate!")
+
